7420e423a45a77ef11d1f4ccdc4568a5473a7a07
Yuzuru Utsunomiya <35525121+yuzuruu@users.noreply.github.com>
2019-10-29 13:58:08 +0900

    added logistic regression

--- water_quality.r
+++ water_quality.r
@@ -28,8 +28,39 @@ print(pairs.water.quality)
 # Save the pair plot
 # ggsave("pairs.water.quality.png")
 
+# ---- logistic.regression ------
+# replace "ps" and "pn" into "1" and "0"
+# glm() function accepts integer for 1-0 
+water.quality$depth[water.quality$depth=="ps"] <- 1 
+water.quality$depth[water.quality$depth=="pn"] <- 0
+# transform the valuable as numeric
+water.quality$depth <- as.numeric(as.character(water.quality$depth))
+water.quality %>% 
+  (mutate_at(.,vars(3:11), funs((.-mean(.))/sd(.))))
+         
+         
+         oxy ec cod bod nitrate amonium sat nhom)
 
-# hoge <- princomp(water.quality[,c(3:11)],
-#                  scale = T
-#                  )
-# summary(hoge)
+
+# regression
+wq.binomial.01 <- glm(depth ~ ph + cod + bod,
+            data = water.quality,
+            family = binomial
+)
+# show summary table
+summary(wq.binomial.01)
+# Akaike's information criterion
+AIC(wq.binomial.01)
+# plot the results
+wq.binomial.01.plot <- 
+  ggplot(water.quality, 
+         aes(x=ph + cod + bod, 
+             y=depth)
+         ) + 
+  geom_point() + 
+  stat_smooth(method="glm", 
+              method.args=list(family="binomial"), 
+              se=TRUE
+              ) +
+  theme_classic()
+
